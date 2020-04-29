ae6425af87f1a038c510c805ac9f2e921aa72c1f
PaulAshraf <paulashraf14@gmail.com>
2020-04-30 01:21:24 +0200

    add blueprint ui

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,85 +1,162 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import { Button, Card, Elevation } from "@blueprintjs/core";
+import styled from "styled-components";
 
-import list from '../list.json'
+import list from "../list.json";
+import "../App.css";
 
 function Main() {
-
   const [movieList, setMovieList] = useState(list.notseen.slice());
   const [movieListSeen, setMovieListSeen] = useState(list.seen.slice());
-  const [num, setNum] = useState(1)
-  
+  const [num, setNum] = useState(1);
+
   return (
     <div>
+      <h1>
+        I want to finish IMDB's Top 250 movies in
+        <input
+          className="months"
+          type="number"
+          id="months"
+          min="1"
+          onChange={(e) => {
+            setNum(Math.ceil((250 - movieListSeen.length) / (30 * e.target.value)));
+            setMovieList(reshape(movieList.slice(), num));
+          }}
+        ></input>
+        months
+      </h1>
+      <button onClick={() => setMovieList(shuffleMovies(movieList))}>
+        Shuffle Order
+      </button>
+      <button onClick={() => setMovieList(list.notseen.slice())}>
+        Order by rating
+      </button>
+      <br /> <br />
+      {movieList.map((movieGroup, i) => {
+console.log(movieList)
+          
+        let today = new Date();
+        if (i !== 0) {
+          today.setDate(today.getDate() + i);
+        }
+        let dd = String(today.getDate()).padStart(2, "0");
+        let mm = String(today.getMonth() + 1).padStart(2, "0");
+        let yyyy = today.getFullYear();
 
-        <h1>I want to finish IMDB's Top 250 movies in {' '} 
-        <input className="months" type="number" id="months" min="1" onChange={(e) => setNum(Math.ceil(movieList.length / (30 * e.target.value)))}></input>
-        {' '} months
-        </h1> 
+        if(!movieGroup[0]){
+            console.log('here')
+            setMovieList(reshape(movieList.slice(), num));
+        }
+            console.log(movieList)
 
-        <button onClick={() => setMovieList(shuffleMovies(movieList))}>Shuffle Order</button>
-        {' '}
-        <button onClick={() => setMovieList(list.notseen.slice())}>Order by rating</button>
 
-        <br /> <br /> 
+        return (
+          <div>
+            <h3>{dd + "/" + mm + "/" + yyyy}</h3>
 
-        <table>
-        <tr> <th>Date</th> <th>Movie</th> <th>Poster</th> <th>{'    '}</th></tr>
-
-        {movieList.map(
-
-          (movie, i) => {
-
-            let today = new Date()
-            if(i % num === 0){
-              today.setDate(today.getDate() + i / num)
-            }
-            let dd = String(today.getDate()).padStart(2, '0')
-            let mm = String(today.getMonth() + 1).padStart(2, '0')
-            let yyyy = today.getFullYear()
-
-            return(
-              <tr>
-                {i % num === 0?<td rowSpan={num}>{dd + '/' + mm + '/' + yyyy}</td>:<></>}
-                <td>{movie.name + ' (' + movie.year + ')'}</td>
-                <td><img src={movie.poster} alt={movie.name} /></td>
-                <td><button onClick={() => {setMovieList(movieList.filter(item => item.index !== movie.index));
-                                            setMovieListSeen(movieListSeen.concat([movie]));
-                                    }}>I saw this</button></td>
-              </tr>
-            )
-
-          })}
-          </table>
-
-          <br />
-
-          <table>
-          <tr> <th>Movie</th> <th>Poster</th> </tr>
-
-          {movieListSeen.map(
-
-            (movie) => {
-
-              return(
-                <tr>
-                  <td>{movie.name + ' (' + movie.year + ')'}</td>
-                  <td><img src={movie.poster} alt={movie.name} /></td>
-                </tr>
-              )
-
-            })}
-            </table>
-
+            <Container key={i}>
+              {movieGroup.map((movie) => {
+                return (
+                  <div key={movie.index}>
+                    <Card interactive={true} elevation={Elevation.FOUR}>
+                      <MovieContainer>
+                        <MoviePoster>
+                          {/* <img src={movie.smallPosterLink} alt={movie.title} /> */}
+                        </MoviePoster>
+                        <MovieInfo>
+                          <h3>{"#" + movie.index + ": " + movie.title}</h3>
+                          {/* <p>{movie.overview}</p> */}
+                          <Buttons>
+                            <Button
+                              intent="danger"
+                              disabled={movie.ytLink === "NA"}
+                              icon="video"
+                            >
+                              {" "}
+                              <ButtonLink href={movie.ytLink}>
+                                Watch Trailer
+                              </ButtonLink>{" "}
+                            </Button>
+                            <Button intent="warning" icon="film">
+                              <ButtonLink href={movie.imdbLink}>
+                                {" "}
+                                IMDB Page{" "}
+                              </ButtonLink>
+                            </Button>
+                            <Button
+                              intent="success"
+                              icon="tick-circle"
+                              onClick={() => {
+                                setMovieList(
+                                  movieList.filter(
+                                    (item) => item.index !== movie.index
+                                  )
+                                );
+                                setMovieListSeen(movieListSeen.concat([movie]));
+                              }}
+                            >
+                              Seen This Movie{" "}
+                            </Button>
+                          </Buttons>
+                        </MovieInfo>
+                      </MovieContainer>
+                    </Card>
+                  </div>
+                );
+              })}
+            </Container>
+          </div>
+        );
+      })}
+      <br />
     </div>
   );
 }
 
-function shuffleMovies(movieList){
+function reshape(list, num) {
+    console.log('reshape:    ', list)
+    console.log('reshape:    ', num)
+
+  let reShapedMovieList = [];
+  while (list.length) reShapedMovieList.push(list.splice(0, num));
+  console.log('after reshape:    ', reShapedMovieList)
+  return reShapedMovieList;
+
+
+}
+
+function shuffleMovies(movieList) {
   for (let i = movieList.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [movieList[i], movieList[j]] = [movieList[j],movieList[i]];
+    [movieList[i], movieList[j]] = [movieList[j], movieList[i]];
   }
-  return movieList.slice()
+  return movieList.slice();
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const MovieContainer = styled.div`
+  display: flex;
+  margin: 1em;
+  flex-basis: 33%;
+`;
+
+const MoviePoster = styled.div`
+  flex-basis: 20%;
+`;
+
+const MovieInfo = styled.div`
+  flex-basis: 80%;
+`;
+const Buttons = styled.div`
+  display: flex;
+`;
+const ButtonLink = styled.a`
+  text-decoration: none;
+`;
 
 export default Main;
